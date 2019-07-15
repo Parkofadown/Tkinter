d227a09c10c533d3b3b4fecd6587bd8ff116a598
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-07-14 20:26:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+amazing_label = Label(root, text="Amazing. This is an awesome label")
+label_2 = Label(root, text = "This is alright..")
+amazing_label.pack()
+label_2.pack()
+root.mainloop()
